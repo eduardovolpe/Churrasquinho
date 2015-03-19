6e89259b30cff5489dca51b7757f6885e5eab3e4
eduardofavarin <edupaulofavarin@gmail.com>
2015-03-19 14:56:30 -0300

    Teste 2

--- app/src/main/java/br/com/e3/churrasquinho/MainActivity.java
+++ app/src/main/java/br/com/e3/churrasquinho/MainActivity.java
@@ -6,8 +6,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
+// import android.view.inputmethod.InputMethodManager;
+// import android.widget.Button;
 import android.widget.ImageView;
 
 
