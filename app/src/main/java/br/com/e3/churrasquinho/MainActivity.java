5535db9a705a12cac2a86a14cc8ffa56206f3277
eduardofavarin <edupaulofavarin@gmail.com>
2015-03-19 14:24:50 -0300

    Teste 2

--- app/src/main/java/br/com/e3/churrasquinho/MainActivity.java
+++ app/src/main/java/br/com/e3/churrasquinho/MainActivity.java
@@ -1,17 +1,44 @@
 package br.com.e3.churrasquinho;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    ImageView btnIniciar;
+    ImageView btnEditar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIniciar = (ImageView) findViewById(R.id.btnIniciar);
+        btnEditar = (ImageView) findViewById(R.id.btnEditar);
+
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, iniciarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, editarActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
