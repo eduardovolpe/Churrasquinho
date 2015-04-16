592dfad27d8045467ea6cd9981e89165012b5c63
eduardofavarin <edupaulofavarin@gmail.com>
2015-04-16 14:41:40 -0300

    Teste 2

--- app/src/main/java/br/com/e3/churrasquinho/iniciarActivity.java
+++ app/src/main/java/br/com/e3/churrasquinho/iniciarActivity.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,7 +24,11 @@ public class iniciarActivity extends ActionBarActivity {
     ImageView menosC;
     ImageView maisC;
 
-    Integer total = 1;
+    Integer total = 0;
+    Integer totalHomem = 0;
+    Integer totalMulher = 0;
+    Integer totalCrianca = 0;
+
 
 
     @Override
@@ -45,59 +48,43 @@ public class iniciarActivity extends ActionBarActivity {
         maisC = (ImageView) findViewById(R.id.maisC);
 
         txtTotalConvidados = (EditText) findViewById(R.id.txtTotalConvidados);
+        txtTotalConvidados.setText("Total de Convidados: 0");
 
-        edtCrianca.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        totalMulher = Integer.parseInt(edtMulher.getText().toString());
+        totalHomem = Integer.parseInt(edtHomem.getText().toString());
+        totalCrianca = Integer.parseInt(edtCrianca.getText().toString());
+
+        maisH.setOnClickListener(new View.OnClickListener() {
             @Override
-                    public void onFocusChange(View v, boolean hasFocus) {
-                        aumentar(Integer.parseInt(edtCrianca.getText().toString()));
-                    }
-        });
-
-        edtCrianca.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-
-                aumentar(Integer.parseInt(edtCrianca.getText().toString()));
-                return false;
-            }
-        });
-
-        maisH.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Integer totalHomem =  Integer.parseInt(edtHomem.getText().toString());
-                totalHomem ++;
-                aumentar();
+            public void onClick(View v) {
+               totalHomem++;
                 edtHomem.setText(totalHomem.toString());
-                return true;
+                total = totalHomem + totalMulher + totalCrianca;
+
+                atualizaTotal(total);
             }
         });
 
         menosH.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Short totalHomem =  Short.parseShort(edtHomem.getText().toString());
-
-                if (totalHomem > 0) {
-                    totalHomem --;
-                    diminuir();
-                } else {
-                    totalHomem = 0;
-                }
-
+                totalHomem--;
                 edtHomem.setText(totalHomem.toString());
 
+                total = totalHomem + totalMulher + totalCrianca;
+
+                atualizaTotal(total);
             }
         });
 
         maisM.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Short totalMulher =  Short.parseShort(edtMulher.getText().toString());
-                totalMulher ++;
-                aumentar();
+                totalMulher++;
                 edtMulher.setText(totalMulher.toString());
 
+                total = totalHomem + totalMulher + totalCrianca;
+                atualizaTotal(total);
             }
         });
 
@@ -105,15 +92,11 @@ public class iniciarActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
 
-                Short totalMulher =  Short.parseShort(edtMulher.getText().toString());
-                if (totalMulher > 0) {
-                    totalMulher --;
-                    diminuir();
-                } else {
-                    totalMulher = 0;
-                }
-
+                totalMulher--;
                 edtMulher.setText(totalMulher.toString());
+
+                total = totalHomem + totalMulher + totalCrianca;
+                atualizaTotal(total);
 
             }
         });
@@ -121,36 +104,31 @@ public class iniciarActivity extends ActionBarActivity {
         maisC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Short totalCrianca =  Short.parseShort(edtCrianca.getText().toString());
-                totalCrianca ++;
-                aumentar();
-                edtCrianca.setText(totalCrianca.toString());
+               totalCrianca++;
+               edtCrianca.setText(totalCrianca.toString());
 
+               total = totalHomem + totalMulher + totalCrianca;
+                atualizaTotal(total);
             }
         });
 
         menosC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Short totalCrianca =  Short.parseShort(edtCrianca.getText().toString());
-                if (totalCrianca > 0) {
-                    totalCrianca--;
-                    diminuir();
-                } else {
-                    totalCrianca = 0;
-                }
+                totalCrianca--;
                 edtCrianca.setText(totalCrianca.toString());
-
+                total = totalHomem + totalMulher + totalCrianca;
+                atualizaTotal(total);
             }
         });
 
         // MessageBox aparece caso nenhum convidado seja colocado e usuário queira prosseguir
         final AlertDialog alertaConvidados = new AlertDialog.Builder(this).create();
         alertaConvidados.setTitle("Nenhum Convidado");
-        alertaConvidados.setMessage("Churrasco sem convidados não dá, né.\nConvide alguém aí!");
+        alertaConvidados.setMessage("Churrasquear sozinho não dá, né.\nConvide alguém aí!");
         alertaConvidados.setButton("OK", new DialogInterface.OnClickListener() {
-             public void onClick(DialogInterface dialog, int which){}
+            public void onClick(DialogInterface dialog, int which) {
+            }
         });
         alertaConvidados.setIcon(R.drawable.ic_launcher);
 
@@ -168,22 +146,10 @@ public class iniciarActivity extends ActionBarActivity {
         });
     }
 
-    private void aumentar(){
-        total++;
-        txtTotalConvidados.setText(String.valueOf("Total de Convidados: " + total));
-    }
-    
-    private void aumentar(Integer ttl){
-        total += ttl;
+    public void atualizaTotal(int total){
         txtTotalConvidados.setText(String.valueOf("Total de Convidados: " + total));
     }
 
-    private void diminuir(){
-        total--;
-        txtTotalConvidados.setText(String.valueOf("Total de Convidados: " + total));
-    }
 
-    private void zerar(Integer ttl){
-        total = 0;
-    }
 }
+
