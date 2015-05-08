990f41f76e9990a953568d29ec7762d3f28d49f3
eduardofavarin <edupaulofavarin@gmail.com>
2015-05-08 14:39:54 -0300

    Teste 2

--- app/src/main/java/br/com/e3/churrasquinho/acompanhamentoActivity.java
+++ app/src/main/java/br/com/e3/churrasquinho/acompanhamentoActivity.java
@@ -1,7 +1,10 @@
 package br.com.e3.churrasquinho;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.List;
@@ -11,6 +14,9 @@ public class acompanhamentoActivity extends ActionBarActivity {
 
     ListView lstAcompanhamento;
     DBAdapterAcompanhamento dbAdapterAcompanhamento;
+    Button btnInserir;
+    Button btnProsseguir;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +24,39 @@ public class acompanhamentoActivity extends ActionBarActivity {
         setContentView(R.layout.activity_acompanhamento);
 
         lstAcompanhamento = (ListView) findViewById(R.id.lstAcompanhamentos);
+        btnInserir = (Button) findViewById(R.id.btnCadAcompanhamento);
+        btnProsseguir = (Button) findViewById(R.id.btnProsAcompanhamentos);
 
         dbAdapterAcompanhamento = new DBAdapterAcompanhamento(this);
         dbAdapterAcompanhamento.open();
 
-        List<Acompanhamento> acompanhamentos = dbAdapterAcompanhamento.listar();
+        final List<Acompanhamento> acompanhamentos = dbAdapterAcompanhamento.listar();
         AdapterListAcompanhamento adapter = new AdapterListAcompanhamento(this, acompanhamentos);
 
         lstAcompanhamento.setAdapter(adapter);
 
         dbAdapterAcompanhamento.close();
+
+        btnInserir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(acompanhamentoActivity.this, InserirCarne.class);
+
+                Bundle param = new Bundle();
+                param.putString("texto", "acompanhamento");
+                intent.putExtras(param);
+
+                startActivity(intent);
+            }
+        });
+
+        btnProsseguir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(acompanhamentoActivity.this, outroActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/br/com/e3/churrasquinho/outroActivity.java
+++ app/src/main/java/br/com/e3/churrasquinho/outroActivity.java
@@ -1,7 +1,10 @@
 package br.com.e3.churrasquinho;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.List;
@@ -11,6 +14,9 @@ public class outroActivity extends ActionBarActivity {
 
     ListView lstOutro;
     DBAdapterOutro dbAdapterOutro;
+    Button btnInserir;
+    Button btnProsseguir;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,8 @@ public class outroActivity extends ActionBarActivity {
         setContentView(R.layout.activity_outro);
 
         lstOutro = (ListView) findViewById(R.id.lstOutro);
+        btnInserir = (Button) findViewById(R.id.btnCadOutro);
+        btnProsseguir = (Button) findViewById(R.id.btnProsOutro);
 
         dbAdapterOutro = new DBAdapterOutro(this);
         dbAdapterOutro.open();
@@ -26,8 +34,25 @@ public class outroActivity extends ActionBarActivity {
         AdapterListOutro adapter = new AdapterListOutro(this, outro);
 
         lstOutro.setAdapter(adapter);
-
         dbAdapterOutro.close();
 
+        btnInserir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(outroActivity.this, InserirCarne.class);
+                Bundle param = new Bundle();
+                param.putString("texto", "outro");
+                intent.putExtras(param);
+                startActivity(intent);
+            }
+        });
+        btnProsseguir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(outroActivity.this, ResumoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
