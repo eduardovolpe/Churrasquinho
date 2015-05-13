f9205a2256e5bf274eb9cfa835b9c01b0d25e312
eduardofavarin <edupaulofavarin@gmail.com>
2015-05-13 14:07:03 -0300

    Teste 2

--- app/src/main/java/br/com/e3/churrasquinho/ResumoActivity.java
+++ app/src/main/java/br/com/e3/churrasquinho/ResumoActivity.java
@@ -1,17 +1,32 @@
 package br.com.e3.churrasquinho;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class ResumoActivity extends ActionBarActivity {
+
+    Button btnProsseguir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo);
+
+        btnProsseguir = (Button) findViewById(R.id.btnProsResumo);
+
+        btnProsseguir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ResumoActivity.this, FinalizarActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
--- app/src/main/java/br/com/e3/churrasquinho/iniciarActivity.java
+++ app/src/main/java/br/com/e3/churrasquinho/iniciarActivity.java
@@ -5,6 +5,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -151,6 +153,28 @@ public class iniciarActivity extends ActionBarActivity {
         txtTotalConvidados.setText(String.valueOf("Total de Convidados: " + total));
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_iniciar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 }
 
